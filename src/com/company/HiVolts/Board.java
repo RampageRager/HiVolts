8ba3056dc6450b0ab08bde2524b3cf1a42a42ceb
Keerath <keerath@Keeraths-MacBook-Air.local>
2021-09-18 16:19:31 -0700

    change board class

--- src/com/company/HiVolts/Board.java
+++ src/com/company/HiVolts/Board.java
@@ -1,30 +1,30 @@
 package com.company.HiVolts;
 
 public class Board {
-    private int[] myArr = new int[5];
     private Cell[][] board = new Cell[12][12];
 
-    public boolean hasPlayer (int i, int j) {
-        return board[i][j].hasPlayer();
+    public boolean hasPlayer(int i, int j) {
+        return board[i][j].checkPlayer();
     }
-
-    public fence getFence(int i, int j) {
-        return board[i][j].getFence();
-    }
-
-    public m
 
     public Player getPlayer(int i, int j) {
-        return board[i][j].getPlayer();
+        return board[i][j].returnPlayer();
     }
 
-    public void setElement (int index) {
-        myArr[index] = 3;
+    public boolean hasFence(int i, int j) {
+        return board[i][j].checkFence();
     }
 
-    public void printArr() {
-        for (int i = 0; i < myArr.length; i++) {
-            System.out.println(myArr[i]);
-        }
+    public Fence returnFence(int i, int j) {
+        return board[i][j].returnFence();
     }
+
+    public boolean hasMho(int i, int j) {
+        return board[i][j].checkMho();
+    }
+
+    public Mho returnMho(int i, int j) {
+        return board[i][j].returnMho();
+    }
+
 }
