83990c5cc7ab43aaca620c27d7f481b30227242a
Linden Wang <linden@Lindens-MacBook-Air.local>
2021-09-18 15:44:29 -0700

    Added to Mho Class

--- src/com/company/HiVolts/Mho.java
+++ src/com/company/HiVolts/Mho.java
@@ -3,12 +3,24 @@ package com.company.HiVolts;
 public class Mho {
         private int mhoCount;
         private int validMoves;
-        private String mhoLocation;
-        private String moveResult;
-        public Mho(int Mhocount, int validMoves, String mhoLocation, String moveResult){
+        private int mhoLocation;
+        private int moveResult;
+        public Mho(int Mhocount, int validMoves, int mhoLocation, int moveResult){
             this.mhoCount = mhoCount;
             this.validMoves = validMoves;
             this.mhoLocation = mhoLocation;
             this.moveResult = moveResult;
+    }
+    public int getMhoCount(){
+            return this.mhoCount;
+    }
+    public int getValidMoves(){
+            return this.validMoves
+    }
+    public int setMhoLocation(int mhoLocation) {
+        this.setMhoLocation(); = mhoLocation[0][0];
+    }
+    public int getMoveResult(){
+            return this.moveResult;
     }
 }
