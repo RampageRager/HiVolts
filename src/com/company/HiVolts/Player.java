8de9b2a0afeec8f7e81a66986d3b813f01b52fe9
RampageRager <alekseyvalouev@gmail.com>
2021-09-18 16:41:44 -0700

    added player class

--- src/com/company/HiVolts/Player.java
+++ src/com/company/HiVolts/Player.java
@@ -1,4 +1,25 @@
 package com.company.HiVolts;
 
+import java.awt.*;
+
 public class Player {
+    private Point playerCoords;
+    private Point move;
+    public void setPlayerCoords(int i, int j) {
+        playerCoords.setLocation(i, j);
+    }
+    public Point getPlayerCoords() {
+        return playerCoords;
+    }
+
+    public void setMove(int i, int j) {
+        move.setLocation(i, j);
+    }
+    public boolean checkPossible() {
+        if (playerCoords.getX()+move.getX() < 0 || playerCoords.getX()+move.getX() > 11 || playerCoords.getY()+move.getY() < 0 || playerCoords.getY()+move.getY() > 11) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
