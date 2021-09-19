efd30c2a60e30fe3222209578c4bfea75093ad9b
RampageRager <alekseyvalouev@gmail.com>
2021-09-18 21:37:18 -0700

    graphics finished

--- src/com/company/HiVolts/Board.java
+++ src/com/company/HiVolts/Board.java
@@ -1,6 +1,7 @@
 package com.company.HiVolts;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Board {
     private Cell[][] board = new Cell[12][12];
@@ -8,17 +9,123 @@ public class Board {
     private Point move;
 
     public Board() {
+        Random rand = new Random();
+        Point[] fenceLocs = new Point[20];
+        Point[] mhoLocs = new Point[12];
+        Point playerLoc = null;
+        boolean matches = false;
+        while (fenceLocs[19] == null) {
+            int nullInd = 0;
+            Point newPoint;
+            for (int i = 0; i < 20; i++) {
+                if (fenceLocs[i] == null) {
+                    nullInd = i;
+                    break;
+                }
+            }
+            newPoint = new Point(rand.nextInt(10)+1, rand.nextInt(10)+1);
+            for (int i = 0; i < 20; i++) {
+                if (fenceLocs[i] != null) {
+                    if (fenceLocs[i].getX() == newPoint.getX() && fenceLocs[i].getY() == newPoint.getY()) {
+                        matches = true;
+                        break;
+                    }
+                } else {
+                    matches = false;
+                }
+            }
+            if (matches == false) {
+                fenceLocs[nullInd] = newPoint;
+            }
+        }
+
+        while (mhoLocs[11] == null) {
+            matches = false;
+            int nullInd = 0;
+            Point newPoint;
+            for (int i = 0; i < 12; i++) {
+                if (mhoLocs[i] == null) {
+                    nullInd = i;
+                    break;
+                }
+            }
+            newPoint = new Point(rand.nextInt(10)+1, rand.nextInt(10)+1);
+            for (int i = 0; i < 12; i++) {
+                if (mhoLocs[i] != null) {
+                    if (mhoLocs[i].getX() == newPoint.getX() && mhoLocs[i].getY() == newPoint.getY()) {
+                        matches = true;
+                        break;
+                    }
+
+                } else {
+                    matches = false;
+                }
+
+                for (int j = 0; j < 20; j++) {
+                    if (newPoint.getX() == fenceLocs[j].getX() && newPoint.getY() == fenceLocs[j].getY()) {
+                        matches = true;
+                        break;
+                    }
+                }
+            }
+            if (matches == false) {
+                mhoLocs[nullInd] = newPoint;
+            }
+
+
+        }
+
+        while (playerLoc == null) {
+            matches = false;
+            Point newPoint;
+            newPoint = new Point(rand.nextInt(10)+1, rand.nextInt(10)+1);
+
+            for (int i = 0; i < 12; i++) {
+                if (mhoLocs[i].getX() == newPoint.getX() && mhoLocs[i].getY() == newPoint.getY()) {
+                    matches = true;
+                    break;
+                }
+            }
+            for (int j = 0; j < 20; j++) {
+                if (newPoint.getX() == fenceLocs[j].getX() && newPoint.getY() == fenceLocs[j].getY()) {
+                    matches = true;
+                    break;
+                }
+            }
+            if (matches == false) {
+                playerLoc = newPoint;
+            }
+        }
+
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
-                if (i == 0 || i == 12) {
+                board[i][j] = new Cell();
+                if (i == 0 || i == 11) {
                     board[i][j].setFence(new Fence());
-                } else if (j == 0 || j == 12) {
+                } else if (j == 0 || j == 11) {
                     board[i][j].setFence(new Fence());
-                } else {
-                    board[i][j].setMho(new Mho(new Point(i, j)));
                 }
             }
         }
+
+        for (int i = 0; i < 20; i++) {
+            Point fenceLoc = fenceLocs[i];
+            int px = (int) fenceLoc.getX();
+            int py = (int) fenceLoc.getY();
+            board[px][py].setFence(new Fence());
+        }
+
+        for (int i = 0; i < 12; i++) {
+            Point mhoLoc = mhoLocs[i];
+            int px = (int) mhoLoc.getX();
+            int py = (int) mhoLoc.getY();
+            board[px][py].setMho(new Mho(mhoLoc));
+        }
+
+        int px = (int) playerLoc.getX();
+        int py = (int) playerLoc.getY();
+        board[px][py].setPlayer(new Player());
+
     }
 
     public Cell[][] getBoard() {
--- src/com/company/HiVolts/GUI.java
+++ src/com/company/HiVolts/GUI.java
@@ -2,20 +2,52 @@ package com.company.HiVolts;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JPanel {
+public class GUI extends JPanel implements ActionListener {
     Board myBoard = new Board();
+    Cell[][] grid;
+    Timer updateTimer = new Timer(1, this);
     public GUI() {
         setBorder(BorderFactory.createLineBorder(Color.black));
         this.setBackground(Color.LIGHT_GRAY);
+        grid = myBoard.getBoard();
+        updateTimer.start();
 
     }
     public Dimension getPreferredSize() {
-        return new Dimension(800, 800);
+        return new Dimension(720, 720);
     }
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (myBoard.hasFence(i, j)) {
+                    g.setColor(Color.pink);
+                    g.fillRect(i*60, j*60, 60, 60);
+                }
+                if (myBoard.hasMho(i, j)) {
+                    g.setColor(Color.RED);
+                    g.fillRect(i*60, j*60, 60, 60);
+                }
+                if (myBoard.hasPlayer(i, j)) {
+                    g.setColor(Color.BLUE);
+                    g.fillRect(i*60+20, j*60+20, 20, 20);
+                }
 
+                g.setColor(Color.black);
+                g.drawRect(i*60, j*60, 60, 60);
+            }
+        }
+
+    }
+
+
+    public void actionPerformed(ActionEvent ev) {
+        if (ev.getSource() == updateTimer) {
+            repaint();
+        }
     }
 }
